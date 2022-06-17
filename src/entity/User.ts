05ec21fdb64d0afbf320db120f7a15525a9a692f
Sergey M <marchuk.se@gmail.com>
2022-06-18 00:27:56 +0700

    init: CRUD API

---
+++ src/entity/User.ts
@@ -0,0 +1,21 @@
+// Users are stored as objects that have following properties:
+//     id — unique identifier (string, uuid) generated on server side
+// username — user's name (string, required)
+// age — user's age (number, required)
+// hobbies — user's hobbies (array of strings or empty array, required)
+
+class User {
+    id: string;
+    username: string;
+    age: number;
+    hobbies: string[];
+
+    constructor(username: string, age: number, hobbies: string[]) {
+        this.username = username;
+        this.age = age;
+        this.hobbies = hobbies;
+    }
+    //TODO overload constructors
+    //TODO private fields
+
+}
